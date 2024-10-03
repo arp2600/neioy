2ece4fa3ede61c00e975bd7e7a4b6b547da682ae
George Roger <georger@graphcore.ai>
2024-10-03 14:51:20 +0100

    Add a default clock to the supercollider Server.

--- neioy/supercollider.py
+++ neioy/supercollider.py
@@ -4,6 +4,7 @@ from contextlib import contextmanager
 
 from supriya.enums import RequestName
 from supriya.osc import HealthCheck, OscMessage, OscBundle, ThreadedOscProtocol
+from .clocks import TempoClock
 
 DEFAULT_GROUP = 1
 
@@ -94,7 +95,11 @@ class _BundleMessageSender:
 
 class Server:
     def __init__(self, clock=None):
-        self._clock = clock
+        if clock is None:
+            self._clock = TempoClock()
+        else:
+            self._clock = clock
+
         self.latency = 0.25
 
         self._next_uid = 1234
@@ -106,6 +111,9 @@ class Server:
         )
 
         self._send_message = _MessageSender(self._osc_protocol)
+
+    def get_clock(self):
+        return self._clock
 
     @contextmanager
     def bind(self):
