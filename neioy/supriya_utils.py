6f722ff74f536861d16fd077e90454f127ba3e1f
George <arp2600909@googlemail.com>
2025-09-16 16:05:47 +0100

    Add `add_synthdef` function to supriya utils

--- neioy/supriya_utils.py
+++ neioy/supriya_utils.py
@@ -1,5 +1,7 @@
+import tempfile
 from functools import partialmethod
 from supriya import Group, Server
+from supriya.ugens import compile_synthdefs
 
 
 # Override default_group so the id matches supercolliders.
@@ -8,7 +10,7 @@ def _server_default_group_override(self):
     """
     Get the server's default group.
     """
-    return Group(context=self, id_=(2 ** 26) * self._client_id + 1)
+    return Group(context=self, id_=(2**26) * self._client_id + 1)
 
 
 # Override the _resolve_node method to use the default_group property
@@ -37,10 +39,36 @@ def _server_connect_wrapper(server, connect_func, ip_address, port):
     return server
 
 
-"""
-Apply overrides of supriya functions and methods.
-"""
 def apply_supriya_patches():
+    """
+    Apply overrides of supriya functions and methods.
+    """
     Server.default_group = _server_default_group_override
     Server._resolve_node = _server_resolve_node_override
     Server.connect = partialmethod(_server_connect_wrapper, Server.connect)
+
+
+def add_synthdef(server, synthdef):
+    """
+    Add a syndef to the server, with handling for synthdefs too large to be sent via osc.
+    """
+    # Try using server.add_synthdef. If the synthdef is too large, this will raise an
+    # OSError with a message containing 'Message too long'.
+    try:
+        return server.add_synthdefs(synthdef)
+    except OSError as e:
+        # For any error other than 'Message too long' we just reraise the error.
+        if 'Message too long' not in str(e):
+            raise
+
+    # The synthdef was too large to send using server.add_synthdef, so we save it to a
+    # file and tell the server to load it.
+    print(
+        f"SynthDef {synthdef.name} too big for sending. Retrying via synthdef file"
+    )
+    data = compile_synthdefs(synthdef)
+    with tempfile.NamedTemporaryFile(suffix='.scsyndef') as f:
+        f.write(data)
+        result = server.load_synthdefs(f.name)
+        server.sync()
+        return result
